6acdcd1438f70611f3f0e5b8c042c8e0ba16a0bf
onesliu <onesliu@gmail.com>
2014-04-15 11:54:51 +0800

    temp-ci

--- catalog/controller/weixin/menu_create.php
+++ catalog/controller/weixin/menu_create.php
@@ -23,6 +23,7 @@ $menu_def = '{
 
 class ControllerWeixinMenuCreate extends Controller {
 	public function index() {
+		global $menu_def;
 		
 		// 读取微信配置信息
 		$token = $this->config->get('weixin_token');
@@ -47,18 +48,21 @@ class ControllerWeixinMenuCreate extends Controller {
 		}
 		
 		//创建菜单
-		$url = "https://api.weixin.qq.com/cgi-bin/menu_create/create?access_token=$access_token";
+		$url = "https://api.weixin.qq.com/cgi-bin/menu/create?access_token=$access_token";
 		$ch = curl_init();
 		curl_setopt($ch, CURLOPT_URL, $url);
 		curl_setopt($ch, CURLOPT_POST, true);
+		curl_setopt($ch, CURLOPT_BINARYTRANSFER, true);
 		curl_setopt($ch, CURLOPT_HEADER, false);
 		curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 		curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
 		curl_setopt($ch, CURLOPT_USERAGENT, 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.1 Safari/537.11');
+		curl_setopt($ch, CURLOPT_POSTFIELDS, $menu_def);
 		$res = curl_exec($ch);
 		$rescode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
 		curl_close($ch);
-		
+		$this->log->write($url);
+		$this->log->write($rescode." ".$res);
 		$this->response->setOutput($res);
 		/*
 		$result = json_decode($res);
