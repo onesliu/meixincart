3966f6637bb946f80293ed4f72c965401a366c6b
onesliu <onesliu@gmail.com>
2014-04-17 08:41:07 +0800

    temp-ci

--- system/library/customer.php
+++ system/library/customer.php
@@ -97,7 +97,7 @@ class Customer {
 			$this->newsletter = $customer_query->row['newsletter'];
 			$this->customer_group_id = $customer_query->row['customer_group_id'];
 			$this->address_id = $customer_query->row['address_id'];
-          	
+          	//$registry->get('log')($this->request);
 			$this->db->query("UPDATE " . DB_PREFIX . "customer SET ip = '" . $this->db->escape($this->request->server['REMOTE_ADDR']) . "' WHERE customer_id = '" . (int)$this->customer_id . "'");
 			
 	  		return true;
