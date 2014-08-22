1a4399c337550271f7e24d21f2d7f24557ff7ec5
onesliu <onesliu@gmail.com>
2014-08-22 12:14:54 +0800

    temp

--- pay/weixin.php
+++ pay/weixin.php
@@ -111,15 +111,73 @@ set_error_handler('error_handler');
 // Request
 $request = new Request();
 $registry->set('request', $request);
-
+ 
 // Response
 $response = new Response();
 $response->addHeader('Content-Type: text/html; charset=utf-8');
+$response->setCompression($config->get('config_compression'));
 $registry->set('response', $response); 
+		
+// Cache
+$cache = new Cache();
+$registry->set('cache', $cache); 
 
 // Session
 $session = new Session();
 $registry->set('session', $session);
+
+// Language Detection
+$languages = array();
+
+$query = $db->query("SELECT * FROM `" . DB_PREFIX . "language` WHERE status = '1'"); 
+
+foreach ($query->rows as $result) {
+	$languages[$result['code']] = $result;
+}
+
+$detect = '';
+
+if (isset($request->server['HTTP_ACCEPT_LANGUAGE']) && $request->server['HTTP_ACCEPT_LANGUAGE']) { 
+	$browser_languages = explode(',', $request->server['HTTP_ACCEPT_LANGUAGE']);
+	
+	foreach ($browser_languages as $browser_language) {
+		foreach ($languages as $key => $value) {
+			if ($value['status']) {
+				$locale = explode(',', $value['locale']);
+
+				if (in_array($browser_language, $locale)) {
+					$detect = $key;
+				}
+			}
+		}
+	}
+}
+
+if (isset($session->data['language']) && array_key_exists($session->data['language'], $languages) && $languages[$session->data['language']]['status']) {
+	$code = $session->data['language'];
+} elseif (isset($request->cookie['language']) && array_key_exists($request->cookie['language'], $languages) && $languages[$request->cookie['language']]['status']) {
+	$code = $request->cookie['language'];
+} elseif ($detect) {
+	$code = $detect;
+} else {
+	$code = $config->get('config_language');
+}
+
+if (!isset($session->data['language']) || $session->data['language'] != $code) {
+	$session->data['language'] = $code;
+}
+
+if (!isset($request->cookie['language']) || $request->cookie['language'] != $code) {	  
+	setcookie('language', $code, time() + 60 * 60 * 24 * 30, '/', $request->server['HTTP_HOST']);
+}			
+
+$config->set('config_language_id', $languages[$code]['language_id']);
+$config->set('config_language', $languages[$code]['code']);
+
+// Language	
+$language = new Language($languages[$code]['directory']);
+$language->load($languages[$code]['filename']);	
+$registry->set('language', $language); 
 
 // Document
 $registry->set('document', new Document()); 		
