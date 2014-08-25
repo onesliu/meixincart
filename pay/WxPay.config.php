7cba5232787706a4cac531e1fda55ed54989a789
onesliu <onesliu@gmail.com>
2014-08-25 17:24:08 +0800

    pay demo upload

---
+++ pay/WxPay.config.php
@@ -0,0 +1,12 @@
+<?php
+/**
+* 
+*/
+
+define(APPID , "wx6e583cf65adf0f29");  //appid
+define(APPKEY ,"UdDi6WWTRq"); //paysign key
+define(SIGNTYPE, "sha1"); //method
+define(PARTNERKEY,"7168f80dd0dcc55e417a5afa0431a255");//通加密串
+define(APPSERCERT, "70a757b4b435e250b9cb26c56f13e3b2");
+
+?>
