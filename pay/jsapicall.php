86ddbb5a69cce1fb245b525f9148bfde9d752b4c
onesliu <onesliu@gmail.com>
2014-08-25 17:34:15 +0800

    temp

--- pay/jsapicall.php
+++ pay/jsapicall.php
@@ -8,13 +8,13 @@ $wxPayHelper = new WxPayHelper();
 
 $wxPayHelper->setParameter("bank_type", "WX");
 $wxPayHelper->setParameter("body", "test");
-$wxPayHelper->setParameter("partner", "1900000109");
+$wxPayHelper->setParameter("partner", "1220519101");
 $wxPayHelper->setParameter("out_trade_no", $commonUtil->create_noncestr());
 $wxPayHelper->setParameter("total_fee", "1");
 $wxPayHelper->setParameter("fee_type", "1");
-$wxPayHelper->setParameter("notify_url", "htttp://www.baidu.com");
-$wxPayHelper->setParameter("spbill_create_ip", "127.0.0.1");
-$wxPayHelper->setParameter("input_charset", "GBK");
+$wxPayHelper->setParameter("notify_url", "http://qy.gz.1251102575.clb.myqcloud.com/pay/weixin.php");
+$wxPayHelper->setParameter("spbill_create_ip", $_SERVER['REMOTE_ADDR']);
+$wxPayHelper->setParameter("input_charset", "UTF-8");
 
 
 
