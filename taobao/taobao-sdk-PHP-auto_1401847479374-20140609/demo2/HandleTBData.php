93e4b1447f6ceb0ee432fc8e308a0b00303cef9f
airforcetwo <17570862@qq.com>
2014-09-02 11:58:11 +0800

    更新淘宝访问程序

--- taobao/taobao-sdk-PHP-auto_1401847479374-20140609/demo2/HandleTBData.php
+++ taobao/taobao-sdk-PHP-auto_1401847479374-20140609/demo2/HandleTBData.php
@@ -5,6 +5,7 @@ class HandleTBData
 	private $c;
     private $sessionKey;
     private $resp;
+    private $db;
 
     public function __construct($appkey, $secretkey, $sessionkey)
     {
@@ -12,6 +13,7 @@ class HandleTBData
         $this->c->appkey = $appkey;
         $this->c->secretKey = $secretkey;
         $this->sessionKey = $sessionkey;
+        $this->db = new DB(DB_DRIVER, DB_HOSTNAME, DB_USERNAME, DB_PASSWORD, DB_DATABASE);
     }
 
     /*
@@ -19,8 +21,9 @@ class HandleTBData
      *
      * Param    $json_str    json编码字符串
      * Note:    json解码后的字段中，fields 为必须字段。其他字段可选。
+     *          调用 tb_TradesSoldGetRequest() 后，需立即调用函数 db_TradesSoldGetRequest() 进行数据库操作。
      */
-    public function getTrades($json_str)
+    public function tb_TradesSoldGetRequest($json_str)
     {
         $arr = json_decode($json_str);
 
@@ -39,16 +42,18 @@ class HandleTBData
         if (isset($arr->type))          $req->setType($arr->type);
         if (isset($arr->useHasNext))    $req->setUseHasNext($arr->useHasNext);
 
-        $this->resp = $this->c->execute($req, $this->sessionKey);
+        return $this->resp = $this->c->execute($req, $this->sessionKey);
     }
+
 
     /*
      * 查询卖家已卖出的增量交易数据（根据修改时间）
      *
      * Param    $json_str    json编码字符串
      * Note:    json解码后的字段中，fields，startModified 和 endModified 为必须字段。其他字段可选。
+     *          调用 tb_TradesSoldIncrementGetRequest()后，需立即调用函数 db_TradesSoldIncrementGetRequest() 进行数据库操作。
      */
-    public function getIncrementTrades($json_str)
+    public function tb_TradesSoldIncrementGetRequest($json_str)
     {
         $arr = json_decode($json_str);
 
@@ -65,8 +70,9 @@ class HandleTBData
         if (isset($arr->type))              $req->setType($arr->type);
         if (isset($arr->useHasNext))        $req->setUseHasNext($arr->useHasNext);
 
-        $this->resp = $this->c->execute($req, $this->sessionKey);
+        return $this->resp = $this->c->execute($req, $this->sessionKey);
     }
+
 
     /*
      * 获取单笔交易的详细信息
@@ -74,7 +80,7 @@ class HandleTBData
      * Param    $json_str    json编码字符串
      * Note:    json解码后的字段中，fields 和 tid 为必须字段。其他字段可选。
      */
-    public function getTradeFullInfo($json_str)
+    public function tb_TradeFullinfoGetRequest($json_str)
     {
         $arr = json_decode($json_str);
 
@@ -83,8 +89,9 @@ class HandleTBData
         if (isset($arr->fields))    $req->setFields($arr->fields);
         if (isset($arr->tid))       $req->setTid($arr->tid);
 
-        $this->resp = $this->c->execute($req, $this->sessionKey);
+        return $this->resp = $this->c->execute($req, $this->sessionKey);
     }
+
 
     /*
      * 更新商品价格
@@ -92,7 +99,7 @@ class HandleTBData
      * Param    $json_str    json编码字符串
      * Note:    json解码后的字段中，numIid 为必须字段。其他字段可选。
      */
-    public function updateItemPrice($json_str)
+    public function tb_ItemPriceUpdateRequest($json_str)
     {
         $arr = json_decode($json_str);
 
@@ -147,7 +154,41 @@ class HandleTBData
         if (isset($arr->validThru))                 $req->setValidThru($arr->validThru);
         if (isset($arr->weight))                    $req->setWeight($arr->weight);
 
+        return $this->resp = $this->c->execute($req, $this->sessionKey);
+    }
+
+
+    /*
+     * 获取当前会话用户出售中的商品列表
+     *
+     * Param    $json_str    json编码字符串
+     * Note:    json解码后的字段中，fields 为必须字段。其他字段可选。
+     *          调用 tb_ItemsOnsaleGetRequest()后，需立即调用函数 db_ItemsOnsaleGetRequest() 进行数据库操作。
+     */
+    public function tb_ItemsOnsaleGetRequest($json_str)
+    {
+        $arr = json_decode($json_str);
+
+        $req = new ItemsOnsaleGetRequest;
+
+        if (isset($arr->cid))           $req->setCid($arr->cid);
+        if (isset($arr->endModified))   $req->setEndModified($arr->endModified);
+        if (isset($arr->fields))        $req->setFields($arr->fields);
+        if (isset($arr->hasDiscount))   $req->setHasDiscount($arr->hasDiscount);
+        if (isset($arr->hasShowcase))   $req->setHasShowcase($arr->hasShowcase);
+        if (isset($arr->isCspu))        $req->setIsCspu($arr->isCspu);
+        if (isset($arr->isEx))          $req->setIsEx($arr->isEx);
+        if (isset($arr->isTaobao))      $req->setIsTaobao($arr->isTaobao);
+        if (isset($arr->orderBy))       $req->setOrderBy($arr->orderBy);
+        if (isset($arr->pageNo))        $req->setPageNo($arr->pageNo);
+        if (isset($arr->pageSize))      $req->setPageSize($arr->pageSize);
+        if (isset($arr->q))             $req->setQ($arr->q);
+        if (isset($arr->sellerCids))    $req->setSellerCids($arr->sellerCids);
+        if (isset($arr->startModified)) $req->setCid($arr->startModified);
+
         $this->resp = $this->c->execute($req, $this->sessionKey);
+
+        return $this->resp;
     }
 
     /*
@@ -207,32 +248,24 @@ class HandleTBData
         return $shippingtime;
     }
 
-    public function tb_print()
-    {
-        echo "result:";
-        print_r($this->resp);
-    }
-
     /*
-     * 订单处理流程。其中包括数据库操作。
+     * 订单处理流程。
      *
      * Param    $json_str    json编码字符串
      * Note:    json解码后的字段中，fields，startModified 和 endModified 为必须字段。其他字段可选。
      */
-    public function handleOrders()
+    public function db_TradesSoldGet()
     {
         if ( isset($this->resp->code) )
         {
             printf("\nERROR! [line:%d] code:%d, msg:%s\n", __LINE__, $this->resp->code, $this->resp->msg);
-            return;
+            return $this->resp;
         }
         else if ( $this->resp->total_results == 0 )
         {
             print_r("\n[line:%d] No new order!\n", __LINE__);
-            return;
+            return $this->resp;
         }
-
-        $db = new DB(DB_DRIVER, DB_HOSTNAME, DB_USERNAME, DB_PASSWORD, DB_DATABASE);
 
         foreach($this->resp->trades->trade as $paraValue)
         {
@@ -248,7 +281,7 @@ class HandleTBData
                 case "TRADE_FINISHED":              //(交易成功)
                 {
                     // 查询是否有相同订单号
-                    $query = $db->query("SELECT * FROM oc_order_product WHERE order_id = $paraValue->sid");
+                    $query = $this->db->query("SELECT * FROM oc_order_product WHERE order_id = $paraValue->sid");
                     if ( $query->num_rows > 0 )
                     {
                         print_r("\nError!Same order!\n");
@@ -256,38 +289,38 @@ class HandleTBData
                     }
 
                     // 查询淘宝账号（昵称）是否存在于数据库中
-                    $query = $db->query("SELECT * FROM oc_customer WHERE firstname = '".$paraValue->buyer_nick."' AND customer_type = 2");
+                    $query = $this->db->query("SELECT * FROM oc_customer WHERE firstname = '".$paraValue->buyer_nick."' AND customer_type = 2");
                     if ( $query->num_rows == 0 )
                     {//插入新淘宝账号
-                        $query = $db->query("INSERT INTO oc_customer SET firstname = '".$paraValue->buyer_nick."', customer_type = 2");
-                        $customerid = $db->getLastId();
+                        $query = $this->db->query("INSERT INTO oc_customer SET firstname = '".$paraValue->buyer_nick."', customer_type = 2");
+                        $customerid = $this->db->getLastId();
 
                         //插入新地址到新淘宝账号
-                        $query = $db->query("INSERT INTO oc_address SET customer_id = ".$customerid.
-                                                                        ", address_1 = '".$paraValue->receiver_address.
-                                                                        "', city = '".$paraValue->receiver_city.
-                                                                        "', telephone = '".$paraValue->receiver_telephone."'");
+                        $query = $this->db->query("INSERT INTO oc_address SET customer_id = ".$customerid.
+                                                                            ", address_1 = '".$paraValue->receiver_address.
+                                                                            "', city = '".$paraValue->receiver_city.
+                                                                            "', telephone = '".$paraValue->receiver_telephone."'");
                         //得到新建的地址id
-                        $addressid = $db->getLastId();
+                        $addressid = $this->db->getLastId();
                     }
                     else
                     {// 淘宝账号已存在
                         $customerid = $query->row['customer_id'];
 
                         //该淘宝账户下是否有相同的(收货人姓名&收货地址&收货人电话)在数据库中
-                        $query = $db->query("SELECT * FROM oc_address WHERE customer_id = ".$customerid.
-                                                                            " AND firstname = '".$paraValue->receiver_name.
-                                                                            "' AND address_1 = '".$paraValue->receiver_address.
-                                                                            "' AND telephone = '".$paraValue->receiver_mobile."'");
+                        $query = $this->db->query("SELECT * FROM oc_address WHERE customer_id = ".$customerid.
+                                                                                " AND firstname = '".$paraValue->receiver_name.
+                                                                                "' AND address_1 = '".$paraValue->receiver_address.
+                                                                                "' AND telephone = '".$paraValue->receiver_mobile."'");
                         if ( $query->num_rows == 0 )
                         {
                             //插入新地址到已存在的淘宝账号
-                            $query = $db->query("INSERT INTO oc_address SET customer_id = ".$customerid.
-                                                                            ", firstname = '".$paraValue->receiver_name.
-                                                                            "', address_1 = '".$paraValue->receiver_address.
-                                                                            "', telephone = '".$paraValue->receiver_mobile.
-                                                                            "', city = '".$paraValue->receiver_city."'");
-                            $addressid = $db->getLastId();
+                            $query = $this->db->query("INSERT INTO oc_address SET customer_id = ".$customerid.
+                                                                                ", firstname = '".$paraValue->receiver_name.
+                                                                                "', address_1 = '".$paraValue->receiver_address.
+                                                                                "', telephone = '".$paraValue->receiver_mobile.
+                                                                                "', city = '".$paraValue->receiver_city."'");
+                            $addressid = $this->db->getLastId();
 
                         }
                         else
@@ -297,7 +330,7 @@ class HandleTBData
                     }
 
                     //更新oc_customer表中该淘宝用户的默认address_id
-                    $query = $db->query("UPDATE oc_customer SET address_id = ".$addressid." WHERE customer_id = ".$customerid);
+                    $query = $this->db->query("UPDATE oc_customer SET address_id = ".$addressid." WHERE customer_id = ".$customerid);
 
                     //拿到买家留言信息
                     if ( $paraValue->has_buyer_message === false )
@@ -306,8 +339,8 @@ class HandleTBData
                     }
                     else
                     {
-                        $this->getTradeFullInfo(json_encode(array('fields' => "buyer_message",
-                                                                  'tid' => $paraValue->sid)));
+                        $this->tb_TradeFullinfoGetRequest(json_encode(array('fields' => "buyer_message",
+                                                                            'tid' => $paraValue->sid)));
 
                         if ( isset($this->resp->code) )
                         {
@@ -319,42 +352,42 @@ class HandleTBData
                     //得到收货时间
                     $receivetime = $this->handleBuyerMessage($this->resp->trade->buyer_message);
                     //插入新订单
-                    $query = $db->query("INSERT INTO oc_order SET customer_id = ".$customerid.
-                                                                ", firstname = '".$paraValue->buyer_nick.
-                                                                "', shipping_firstname = '".$paraValue->receiver_name.
-                                                                "', shipping_address_1 = '".$paraValue->receiver_address.
-                                                                "', shipping_city = '".$paraValue->receiver_city.
-                                                                "', comment = '".$this->resp->trade->buyer_message.
-                                                                "', total = ".$paraValue->payment.
-                                                                ", shipping_time = '".$receivetime.
-                                                                "', other_order_id = ".$paraValue->sid.
-                                                                ", shipping_telephone = '".$paraValue->receiver_mobile."'");
-                    $orderid = $db->getLastId();
+                    $query = $this->db->query("INSERT INTO oc_order SET customer_id = ".$customerid.
+                                                                    ", firstname = '".$paraValue->buyer_nick.
+                                                                    "', shipping_firstname = '".$paraValue->receiver_name.
+                                                                    "', shipping_address_1 = '".$paraValue->receiver_address.
+                                                                    "', shipping_city = '".$paraValue->receiver_city.
+                                                                    "', comment = '".$this->resp->trade->buyer_message.
+                                                                    "', total = ".$paraValue->payment.
+                                                                    ", shipping_time = '".$receivetime.
+                                                                    "', other_order_id = ".$paraValue->sid.
+                                                                    ", shipping_telephone = '".$paraValue->receiver_mobile."'");
+                    $orderid = $this->db->getLastId();
 
-                    // 插入产品信息
+                    // 插入商品信息
                     foreach($paraValue->orders->order as $arr)
                     {
-                        $query = $db->query("SELECT * FROM oc_product_description WHERE name = '".$arr->title."'");
+                        $query = $this->db->query("SELECT * FROM oc_product_description WHERE name = '".$arr->title."'");
                         if ( $query->num_rows == 0 )
                         {
-                            $query = $db->query("INSERT INTO oc_order_product SET order_id = ".$orderid.
-                                                                                ", name = '".$arr->title.
-                                                                                "', quantity = ".$arr->num.
-                                                                                ", price = ".$arr->price.
-                                                                                ", total = ".$arr->payment.
-                                                                                ",other_product_id = ".$arr->num_iid);
+                            $query = $this->db->query("INSERT INTO oc_order_product SET order_id = ".$orderid.
+                                                                                    ", name = '".$arr->title.
+                                                                                    "', quantity = ".$arr->num.
+                                                                                    ", price = ".$arr->price.
+                                                                                    ", total = ".$arr->payment.
+                                                                                    ",other_product_id = ".$arr->num_iid);
                         }
                         else
                         {
                             $productid = $query->row['product_id'];
 
-                            $query = $db->query("INSERT INTO oc_order_product SET order_id = ".$orderid.
-                                                                                ", product_id = ".$productid.
-                                                                                ", name = '".$arr->title.
-                                                                                "', quantity = ".$arr->num.
-                                                                                ", price = ".$arr->price.
-                                                                                ", total = ".$arr->payment.
-                                                                                ", other_product_id = ".$arr->num_iid);
+                            $query = $this->db->query("INSERT INTO oc_order_product SET order_id = ".$orderid.
+                                                                                    ", product_id = ".$productid.
+                                                                                    ", name = '".$arr->title.
+                                                                                    "', quantity = ".$arr->num.
+                                                                                    ", price = ".$arr->price.
+                                                                                    ", total = ".$arr->payment.
+                                                                                    ", other_product_id = ".$arr->num_iid);
 
                         }
                     }
@@ -374,6 +407,41 @@ class HandleTBData
                     break;
             }
         }
+
+        return $this->resp;
+    }
+
+
+    /*
+     * 更新 oc_product 表 other_product_id 字段
+     */
+    public function db_ItemsOnsaleGetRequest()
+    {
+        if ( isset($this->resp->code) )
+        {
+            printf("\nERROR! [line:%d] code:%d, msg:%s\n", __LINE__, $this->resp->code, $this->resp->msg);
+            return $this->resp;
+        }
+        else if ( $this->resp->total_results == 0 )
+        {
+            print_r("\n[line:%d] No new order!\n", __LINE__);
+            return $this->resp;
+        }
+
+        foreach($this->resp->items->item as $paraValue)
+        {
+            $query = $this->db->query("SELECT * FROM oc_product_description WHERE name = '".$paraValue->title."'");
+            if ( $query->num_rows != 0 )
+            {
+                $query = $this->db->query("UPDATE oc_product SET other_product_id = ".$paraValue->num_iid." WHERE product_id = ".$query->row['product_id']);
+            }
+        }
+    }
+
+    public function pt()
+    {
+        echo "result:";
+        print_r($this->resp);
     }
 }
 ?>
--- taobao/taobao-sdk-PHP-auto_1401847479374-20140609/demo2/demo2.php
+++ taobao/taobao-sdk-PHP-auto_1401847479374-20140609/demo2/demo2.php
@@ -16,12 +16,105 @@ require_once(DIR_SYSTEM . 'startup.php');
 // Set default GMT
 date_default_timezone_set('PRC');
 
-$data = new HandleTBData("21798867",
-                         "f319e4c8f72f0776ba206e904074a816",
-                         "6101726daf41926692dc098b83e4925e0d1a94c11aaec652025928648");
+function prompt()
+{
+    print_r("\n////////////////////////////////////////////////////////////////////////////////////////////////");
+    print_r("\n * Parameter a : Call tb_getTrades() to get 3 months orders.");
+    print_r("\n * Parameter b : Call tb_getIncrementTrades() to get increment orders.");
+    print_r("\n * Parameter c : Call tb_updateItemPrice() to update price.");
+    print_r("\n * Parameter d : Call tb_getItemsOnsale() to update other_product_id column in oc_product table.");
+    print_r("\n * Parameter d : Call tb_getItemsOnsale() to update other_product_id column in oc_product table.");
+    print_r("\n////////////////////////////////////////////////////////////////////////////////////////////////\n");
+}
 
-$file = "./data";
+if ( isset($_SERVER['argc']) && $_SERVER['argc'] > 1 )
+{
+    prompt();
 
+    $file = "./data";
+
+    $data = new HandleTBData("21798867",
+        "f319e4c8f72f0776ba206e904074a816",
+        "61020124ab8b649426071391bea8607ab459dd612f5d0da2025928648");
+
+    foreach ($_SERVER['argv'] as $value)
+    {
+        switch ($value)
+        {
+            case 'a':
+                if ( !file_exists($file) )
+                {
+                    // order initialization
+                    // Get orders in 3 months
+                    $fields = "has_buyer_message,seller_nick,buyer_nick,title,type,created,sid,tid,seller_rate,buyer_rate,status,
+                     payment,discount_fee,adjust_fee,post_fee,total_fee,pay_time,end_time,modified,consign_time,
+                     buyer_obtain_point_fee,point_fee,real_point_fee,received_payment,commission_fee,pic_path,
+                     num_iid,num_iid,num,price,cod_fee,cod_status,shipping_type,receiver_name,receiver_state,
+                     receiver_city,receiver_district,receiver_address,receiver_zip,receiver_mobile,receiver_phone,
+                     orders.title,orders.pic_path,orders.price,orders.num,orders.iid,orders.num_iid,orders.sku_id,
+                     orders.refund_status,orders.status,orders.oid,orders.total_fee,orders.payment,orders.discount_fee,
+                     orders.adjust_fee,orders.sku_properties_name,orders.item_meal_name,orders.buyer_rate,
+                     orders.seller_rate,orders.outer_iid,orders.outer_sku_id,orders.refund_id,orders.seller_type";
+
+                    $arr["startDate"] = "";
+                    $arr["endDate"] = date("Y-m-d H:i:s");
+
+                    $data->tb_TradesSoldGetRequest(json_encode(array('fields' => $fields, 'endCreated' => $arr["endDate"])));
+                    $data->db_TradesSoldGet();
+
+                    file_put_contents($file, serialize($arr));//写入缓存
+                }
+                break;
+
+            case 'b':
+                if ( file_exists($file) )
+                {
+                    //从文件中读取上次操作时间
+                    $handle = fopen($file, "r");
+                    $arr = unserialize(fread($handle, filesize($file)));
+
+                    // Get increment orders
+                    $fields = "has_buyer_message,seller_nick,buyer_nick,title,type,created,sid,tid,seller_rate,buyer_rate,status,
+                     payment,discount_fee,adjust_fee,post_fee,total_fee,pay_time,end_time,modified,consign_time,
+                     buyer_obtain_point_fee,point_fee,real_point_fee,received_payment,commission_fee,pic_path,
+                     num_iid,num_iid,num,price,cod_fee,cod_status,shipping_type,receiver_name,receiver_state,
+                     receiver_city,receiver_district,receiver_address,receiver_zip,receiver_mobile,receiver_phone,
+                     orders.title,orders.pic_path,orders.price,orders.num,orders.iid,orders.num_iid,orders.sku_id,
+                     orders.refund_status,orders.status,orders.oid,orders.total_fee,orders.payment,orders.discount_fee,
+                     orders.adjust_fee,orders.sku_properties_name,orders.item_meal_name,orders.buyer_rate,
+                     orders.seller_rate,orders.outer_iid,orders.outer_sku_id,orders.refund_id,orders.seller_type";
+
+                    $arr["startDate"] = $arr["endDate"];
+                    $arr["endDate"] = date("Y-m-d H:i:s");
+
+                    $data->tb_TradesSoldIncrementGetRequest(json_encode(array('fields' => $fields,
+                                                                              'startModified' => $arr["startDate"],
+                                                                              'endModified' => $arr["endDate"])));
+                    $data->db_TradesSoldGet();
+
+                    file_put_contents($file, serialize($arr));//写入缓存
+                }
+                break;
+
+            case 'c':
+                $data->tb_ItemPriceUpdateRequest(json_encode(array('numIid' => 38533168596, //黄瓜
+                                                                   'price => 1.8')));
+                break;
+
+            case 'd';
+                $data->tb_ItemsOnsaleGetRequest(json_encode(array('fields' => "num_iid,title")));
+                $data->db_ItemsOnsaleGetRequest();
+                break;
+
+            default:
+                break;
+        }
+
+        $data->pt();
+    }
+}
+
+/*
 if ( !file_exists($file) )
 {
     // order initialization
@@ -62,7 +155,7 @@ else
 
     $data->getIncrementTrades(json_encode(array('fields' => $fields,
                                                 'startModified' =>  $arr["startDate"],
-                                                'endModified' => $arr["startDate"])));
+                                                'endModified' => $arr["endDate"])));
 }
 
 if ( isset($resp->code) )
@@ -80,5 +173,5 @@ else
 }
 
 $data->tb_print();
-
+*/
 ?>
