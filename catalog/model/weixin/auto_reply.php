b36661390ae6eb29418f0791a97ad67ff6ddd645
onesliu <onesliu@gmail.com>
2014-09-30 15:43:34 +0800

    fix bug.

--- catalog/model/weixin/auto_reply.php
+++ catalog/model/weixin/auto_reply.php
@@ -3,9 +3,6 @@
 class ModelWeixinAutoReply extends Model {
 	
 	public function getReplyFromId($ToUserName, $FromUserName, $msg_id) {
-		if ($msg_id == null)
-			return false;
-
 		$q = $this->db->query("select * from ".DB_PREFIX."auto_message where id=".$msg_id);
 		if ($q->num_rows > 0) {
 			if ($q->row['MsgType'] == 'news') {
