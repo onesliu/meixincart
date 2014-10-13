a3c28372de6dfeaeaf757f04062d0100d6305750
onesliu <onesliu@gmail.com>
2014-10-13 19:10:41 +0800

    temp

--- catalog/controller/module/ms_latest.php
+++ catalog/controller/module/ms_latest.php
@@ -57,8 +57,8 @@ class ControllerModuleMSLatest extends Controller {
 				'price'   	 => $price,
 				'special' 	 => $special,
 				'rating'     => $rating,
-				'weight'	 => $result['weight'] . $result['weight_class'],
-				'type'		 => (($result['product_type']==0)?'固定重量':'先称重'),
+				'weight'	 => ((int)$result['weight']) . $result['weight_class'],
+				'type'		 => (($result['product_type']==0)?'每份固定重量':'先称重后付款'),
 				'original_href'    	 => $this->url->link('product/product', 'product_id=' . $result['product_id']),
 				'href'    	 => $this->url->link('mobile_store/product', 'product_id=' . $result['product_id'])
 			);
