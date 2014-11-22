ea1c34961d54d633b10ddb682df489a7954f35f0
onesliu <onesliu@gmail.com>
2014-11-22 16:10:55 +0800

    fix.

--- admin/controller/weixin/menu_create.php
+++ admin/controller/weixin/menu_create.php
@@ -20,8 +20,21 @@ class ControllerWeixinMenuCreate extends ControllerWeixinWeixin {
 	private function prepare_menu_def($menu_def) {
 		$url = "https://open.weixin.qq.com/connect/oauth2/authorize?appid=APPID&response_type=code&scope=snsapi_base&state=1#wechat_redirect&redirect_uri=REDIRECT_URI";
 		$url = str_replace('APPID', $this->config->get('weixin_appid'), $url);
-		$url = str_replace('REDIRECT_URI', urlencode("http://".MY_DOMAIN."/pay/weixin.php?route=weixin/login"), $url);
-		return str_replace('AUTO_LOGIN:', $url, $menu_def);
+		
+		$u = "http://".MY_DOMAIN."/pay/weixin.php?route=weixin/login";
+		$ret = preg_match_all("/\"AUTO_LOGIN:(.*)\"/", $menu_def, $matches);
+		if ($ret > 0) {
+		        foreach($matches[1] as $param) {
+		                if (strlen($param) > 0)
+		                        $link = $u."&jump=$param";
+		                else
+		                        $link = $u;
+		                $link = str_replace('REDIRECT_URI', urlencode($link), $url);
+		                $menu_def = str_replace("\"AUTO_LOGIN:$param\"", '"'.$link.'"', $menu_def);
+		        }
+		}
+		
+		return $menu_def;
 	}
 }
 ?>
