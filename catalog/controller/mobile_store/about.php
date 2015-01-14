48c27961ec8d3fdda2efc88accd69cbb9d2c3329
onesliu <onesliu@gmail.com>
2015-01-15 06:41:09 +0800

    关于里加入试运营公告

--- catalog/controller/mobile_store/about.php
+++ catalog/controller/mobile_store/about.php
@@ -3,6 +3,13 @@ class ControllerMobileStoreAbout extends Controller {
 	public function index() {
 		$this->data['title'] = $this->document->getTitle();
 		
+		if (isset($this->request->server['HTTPS']) && (($this->request->server['HTTPS'] == 'on') || ($this->request->server['HTTPS'] == '1'))) {
+			$dir_img = $this->config->get('config_ssl') . 'image/';
+		} else {
+			$dir_img = $this->config->get('config_url') . 'image/';
+		}
+		$this->data['dir_img'] = $dir_img;
+		
 		if (file_exists(DIR_TEMPLATE . $this->config->get('config_template') . '/template/mobile_store/about.tpl')) {
 			$this->template = $this->config->get('config_template') . '/template/mobile_store/about.tpl';
 		} else {
