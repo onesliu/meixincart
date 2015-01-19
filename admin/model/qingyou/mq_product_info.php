843c9d4361cc835ca40cebef9f4bfa5e928a7f09
airforcetwo <17570862@qq.com>
2015-01-19 15:58:02 +0800

    下载更新商品信息后，恢复hasedit字段

--- admin/model/qingyou/mq_product_info.php
+++ admin/model/qingyou/mq_product_info.php
@@ -3,19 +3,22 @@ class ModelQingyouMqProductInfo extends Model
 {
     public function getProductInfo()
     {
-    	$data = array();
-    	$sql = "select p.isbn as plu_serial_no, p.ean, p.price, pd.name from oc_product as p left join oc_product_description as pd on p.product_id = pd.product_id where hasedit = 1";
+        $data = array();
+        $sql = "select p.isbn as plu_serial_no, p.ean, p.price, pd.name from oc_product as p left join oc_product_description as pd on p.product_id = pd.product_id where hasedit = 1";
 
-		$query = $this->db->query($sql);
-		foreach ($query->rows as $result) {
-			$o = new stdClass();
-			foreach($result as $key => $val) {
-				$o->$key = $val;
-			}
+        $query = $this->db->query($sql);
+        foreach ($query->rows as $result) {
+            $o = new stdClass();
+            foreach($result as $key => $val) {
+                $o->$key = $val;
+            }
 
-			$data[] = $o;
-		}
+            $data[] = $o;
+        }
 
-		return $data;
-	}
+        $sql = "UPDATE oc_product SET hasedit = 0";
+        $query = $this->db->query($sql);
+
+        return $data;
+    }
 }
