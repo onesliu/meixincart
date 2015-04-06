989cf5bd4551b321ffa2935f9e0feaae0fa53f51
onesliu <onesliu@gmail.com>
2015-04-06 12:12:33 +0800

    增加商城维护暂停公告

---
+++ catalog/controller/weixin/closed.php
@@ -0,0 +1,28 @@
+<?php
+
+class ControllerWeixinClosed extends Controller {
+	public function index() {
+    	
+		$this->data['error_msg'] = '菜鸽子商城维护中，暂时停止服务，预计开放时间12小时以后，对此带来的不便敬请谅解。';
+		
+		if (isset($this->request->server['HTTPS']) && (($this->request->server['HTTPS'] == 'on') || ($this->request->server['HTTPS'] == '1'))) {
+			$dir_img = $this->config->get('config_ssl') . 'image/';
+		} else {
+			$dir_img = $this->config->get('config_url') . 'image/';
+		}
+		$this->data['logo'] = $dir_img . 'logo.png';
+		
+		if (file_exists(DIR_TEMPLATE . $this->config->get('config_template') . '/template/weixin/error.tpl')) {
+            $this->template = $this->config->get('config_template') . '/template/weixin/error.tpl';
+		} else {
+            $this->template = 'default/template/weixin/error.tpl';
+        }
+		
+		$this->children = array(
+			'mobile_store/header',
+		);
+
+		$this->response->setOutput($this->render());
+	}
+}
+?>
--- catalog/view/theme/default/template/weixin/error.tpl
+++ catalog/view/theme/default/template/weixin/error.tpl
@@ -1,12 +1,18 @@
 <?php echo $header; ?>
 <body>
-<div data-role="page" id="weixinerrorpage" data-theme="a" data-title="下单结果">
-	<?php echo $titlebar; ?>
+<div data-role="page" id="weixinclosed" data-theme="a" data-title="商城维护">
+	<?php if (isset($titlebar)) echo $titlebar; else { ?>
+	<div data-role="header" data-position="fixed" data-theme="a" data-tap-toggle="false">
+		<h1 style="padding:.4em 0;"><img src="<?php echo $logo; ?>" style="height:25px;" /></h1>
+	</div>
+	<?php } ?>
 	<div data-role="content">
 		<div class="ui-body ui-body-b ui-corner-all">
 			<p><?php echo $error_msg; ?></p>
 		</div>
+		<?php if (isset($continue)) {?>
 		<a href="<?php echo $continue; ?>" class="ui-btn ui-btn-a ui-corner-all"><?php echo $text_continue; ?></a>
+		<?php } ?>
 	</div>
 	<?php if (isset($navi)) echo $navi;?>
 </div>
